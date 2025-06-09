8f840eeb888965c6c82f087564fc037986a25d5b
labhinav <lakkarajabhinav@gmail.com>
2025-06-09 16:47:40 -0700

    insatll command

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -26,3 +26,18 @@ else
     echo "Installing Mysql is Failure"
 fi         
 dnf install mysql -y 
+
+if [ $? -ne 0]
+then 
+    echo "Mysql is not installed .... going to install it"
+    dnf install mysql -y
+    if[ $? -ne 0 ]
+    then 
+        echo "Installing Mysql is ... SUCCESS"
+    else 
+        echo "Installing Mysql is ... FAILURE"
+        exit 1
+    fi 
+else 
+    echo "Mysql is already installed... Nothing to do"        
+fi     

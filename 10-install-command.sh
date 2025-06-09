a09bde9a66d80d1a5750b3ba9be4b45895b3663b
labhinav <lakkarajabhinav@gmail.com>
2025-06-09 16:20:01 -0700

    conditions

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -13,7 +13,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
-    echo:: "Please run this script with root access"
+    echov"ERROR:: Please run this script with root access"
     exit 1
 else
     echo "You are running with root access"

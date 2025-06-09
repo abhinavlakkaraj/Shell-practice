968b33c5adf55d8c65d96e36ffc61739197e2e4f
labhinav <lakkarajabhinav@gmail.com>
2025-06-09 16:18:47 -0700

    insatll command

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -8,3 +8,13 @@ then
 else
     echo "You are running root access"
 fi 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo:: "Please run this script with root access"
+    exit 1
+else
+    echo "You are running with root access"
+fi  
